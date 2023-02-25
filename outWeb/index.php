1979f337366a4063d23c0f34ce5c6eb26cab1de1
Maciej Czechowicz <kayboard2002@gmail.com>
2023-02-25 16:46:36 +0100

    test img

--- outWeb/index.php
+++ outWeb/index.php
@@ -5,12 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>maciDrive</title>
-    <link rel="icon" href="img/mD/ico/logo-color.ico">
+    <link rel="icon" href="img/mD/ico/logo-no-background.ico">
 
     <link rel="stylesheet" href="css/indexStyles.css">
     <script src="../controls/plugins/jquery-min-3.6.3.js"></script>
 </head>
 <body>
-    
+    <img src="img/maciDrive/png/logo-no-background.png">
 </body>
 </html>
