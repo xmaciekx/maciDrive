86a845efcf6ba1ad80c70c26aee6fffe13ddeeaa
Maciej Czechowicz <kayboard2002@gmail.com>
2023-02-26 17:12:30 +0100

    add and attach simple styles and photos library

--- outWeb/index.php
+++ outWeb/index.php
@@ -9,8 +9,33 @@
 
     <link rel="stylesheet" href="css/indexStyles.css">
     <script src="../controls/plugins/jquery-min-3.6.3.js"></script>
+    <link rel="stylesheet" href="css/justifiedGallery.min.css" />
+    <script src="../controls/plugins/jquery.justifiedGallery.min.js"></script>
 </head>
 <body>
-    <img src="img/maciDrive/png/logo-no-background.png">
+    <div id="navBar">
+        <div class="navBarItem">- - -</div>
+        <img class="navBarItem" id="logoNavBar" src="img/maciDrive/png/logo-no-background.png">
+        <div class="navBarItem">- - -</div>
+    </div>
+
+    <div id="photosContainer">
+        <a href="img/testPhotos/DSC_0585.JPG">
+            <img class="photo" alt="Title 2" src="img/testPhotos/DSC_0585.JPG"/>
+        </a>
+        <a href="img/testPhotos/DSC_0598.JPG">
+            <img class="photo" alt="Title 2" src="img/testPhotos/DSC_0598.JPG"/>
+        </a>
+        <a href="img/testPhotos/DSC_0627.JPG">
+            <img class="photo" alt="Title 2" src="img/testPhotos/DSC_0627.JPG"/>
+        </a>
+        <a href="img/testPhotos/DSC_0666.JPG">
+            <img class="photo" alt="Title 2" src="img/testPhotos/DSC_0666.JPG"/>
+        </a>
+        <a href="img/testPhotos/DSC_0666.JPG">
+            <img class="photo" alt="Title 2" src="img/testPhotos/DSC_0666.JPG"/>
+        </a>
+    </div>
+    <script>$("#photosContainer").justifiedGallery();</script>
 </body>
 </html>
